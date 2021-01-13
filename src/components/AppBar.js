4a0851ef9c75c9c928ce49eaf32f68a123132029
pannachow <chowpanna@gmail.com>
2021-01-13 20:31:09 +0200

    add links to app bar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+import Switch from "@material-ui/core/Switch";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Switch from "@material-ui/core/Switch";
+import { makeStyles } from "@material-ui/core/styles";
 import Brightness4OutlinedIcon from "@material-ui/icons/Brightness4Outlined";
 import Brightness5TwoToneIcon from "@material-ui/icons/Brightness5TwoTone";
 import HotelTwoToneIcon from "@material-ui/icons/HotelTwoTone";
@@ -13,16 +16,12 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
+  brand: {
     flexGrow: 1,
     marginLeft: 10,
   },
-  image: {
-    width: "100%",
-    position: "absolute",
+  white: {
+    color: "white",
   },
 }));
 
@@ -33,10 +32,14 @@ export default function ButtonAppBar({ darkMode, setDarkMode }) {
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar>
-          <HotelTwoToneIcon />
-          <Typography variant="h6" className={classes.title}>
-            HOTEL X
-          </Typography>
+          <div className={classes.brand}>
+            <Link color="inherit" underline="none" component={RouterLink} to={"/"}>
+              <Box display="flex" alignItems="center">
+                <HotelTwoToneIcon />
+                <Typography variant="h6">&nbsp;HOTEL X</Typography>
+              </Box>
+            </Link>
+          </div>
 
           {darkMode ? <Brightness4OutlinedIcon /> : <Brightness5TwoToneIcon />}
           <Switch
@@ -47,9 +50,21 @@ export default function ButtonAppBar({ darkMode, setDarkMode }) {
             inputProps={{ "aria-label": "toggle dark mode" }}
           />
 
-          <Button color="inherit">Rooms</Button>
-          <Button color="inherit">Reservations</Button>
-          <Button color="inherit">Contact</Button>
+          <Button color="inherit">
+            <Link color="inherit" underline="none" component={RouterLink} to={"/rooms"}>
+              Rooms
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link color="inherit" underline="none" component={RouterLink} to={"/reservations"}>
+              Reservations
+            </Link>
+          </Button>
+          <Button color="inherit">
+            <Link color="inherit" underline="none" component={RouterLink} to={"/contact"}>
+              Contact
+            </Link>
+          </Button>
         </Toolbar>
       </AppBar>
       <Toolbar />
