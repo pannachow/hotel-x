241e857bb50518104ddbe7a5237ce0df230deeb0
pannachow <chowpanna@gmail.com>
2021-01-14 12:56:04 +0200

    add sitemap

---
+++ scripts/sitemap.js
@@ -0,0 +1,22 @@
+const { SitemapStream, streamToPromise } = require("sitemap");
+const { Readable } = require("stream");
+const fs = require('fs').promises;
+
+const prefix = "/hotel-x/#";
+const links = [
+    { url: `${prefix}/`, changefreq: "daily", priority: 1.0 },
+    { url: `${prefix}/rooms`, changefreq: "daily", priority: 0.8 },
+    { url: `${prefix}/reservations`, changefreq: "weekly", priority: 0.5 },
+    { url: `${prefix}/contact`, changefreq: "weekly", priority: 0.2 },
+];
+
+(async function() {
+    // create output stream
+    const stream = new SitemapStream({ hostname: "https://pannachow.io" });
+
+    // generate sitemap
+    const buffer = await streamToPromise(Readable.from(links).pipe(stream));
+
+    // write to file
+    await fs.writeFile("public/sitemap.xml", buffer);
+})();
