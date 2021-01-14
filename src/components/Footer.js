d46fece8cb3e1622fe7518338baa88f550480b84
pannachow <chowpanna@gmail.com>
2021-01-14 15:51:24 +0200

    make footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import { useTheme } from "@material-ui/core/styles";
@@ -17,62 +18,66 @@ const styles = {
   socialMedia: {
     fontSize: 28,
   },
+  floatLeft: {
+    float: "left",
+  },
 };
 
 export default function Footer() {
   const theme = useTheme();
-  const links = [
+  const getAroundLinks = [
     { name: "Home", to: "/" },
     { name: "Rooms", to: "/rooms" },
     { name: "Reservations", to: "/reservations" },
     { name: "Contact", to: "/contact" },
   ];
+  const socialMediaLinks = [
+    { href: "https://wa.link/osc6ez", type: WhatsAppIcon },
+    { href: "https://www.facebook.com/ymchowym", type: FacebookIcon },
+    { href: "https://www.instagram.com/miss.pannapanna", type: InstagramIcon },
+    { href: "mailto:chowpanna@gmail.com", type: MailOutlineIcon },
+    { href: "https://pannachow.io", type: LanguageIcon },
+  ];
 
   return (
     <Box bgcolor={theme.palette.type === "dark" ? "background.paper" : "primary.light"} py={6}>
       <Container>
-        <Box display="flex" justifyContent="space-around">
-          <Link component={RouterLink} to={"/"} color="inherit" underline="none">
-            <HotelLogo />
-          </Link>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <Link component={RouterLink} to={"/"} color="inherit" underline="none">
+              <HotelLogo style={styles.floatLeft} />
+            </Link>
+          </Grid>
 
-          <div>
+          <Grid item xs={12} sm={4}>
             <Typography variant="h6">Get around</Typography>
-            {links.map((link) => (
+            {getAroundLinks.map((link) => (
               <Typography key={link.to} variant="subtitle1">
                 <Link component={RouterLink} to={link.to} color="inherit">
                   {link.name}
                 </Link>
               </Typography>
             ))}
-          </div>
+          </Grid>
 
-          <div>
+          <Grid item xs={12} sm={4}>
             <Typography variant="h6">Contact</Typography>
             <Box display="flex" my={1}>
               <RoomIcon />
               <Typography variant="subtitle1">Amsterdam, The Netherlands</Typography>
             </Box>
 
-            <Box display="flex" justifyContent="space-between" pr={6}>
-              <Link href="https://wa.link/osc6ez" color="inherit">
-                <WhatsAppIcon style={styles.socialMedia} />
-              </Link>
-              <Link href="https://www.facebook.com/ymchowym/" color="inherit">
-                <FacebookIcon  style={styles.socialMedia} />
-              </Link>
-              <Link href="https://www.instagram.com/miss.pannapanna/" color="inherit">
-                <InstagramIcon  style={styles.socialMedia} />
-              </Link>
-              <Link href="mailto:chowpanna@gmail.com" color="inherit">
-                <MailOutlineIcon  style={styles.socialMedia} />
-              </Link>
-              <Link href="https://pannachow.io" color="inherit">
-                <LanguageIcon  style={styles.socialMedia} />
-              </Link>
+            <Box display="flex">
+              {socialMediaLinks.map((link) => (
+                <Box mr={4}>
+                  <Link href={link.href} color="inherit">
+                    <link.type style={styles.socialMedia} />
+                  </Link>
+                </Box>
+              ))}
             </Box>
-          </div>
-        </Box>
+          </Grid>
+        </Grid>
       </Container>
     </Box>
   );
--- src/components/HotelLogo.js
+++ src/components/HotelLogo.js
@@ -1,6 +1,6 @@
 import Box from "@material-ui/core/Box";
 
-export default function HotelLogo() {
+export default function HotelLogo({ style }) {
   return (
     <Box
       fontSize={16}
@@ -8,6 +8,7 @@ export default function HotelLogo() {
       display="flex"
       flexDirection="column"
       alignItems="center"
+      style={style}
     >
       <img src="hotel-x-icon.png" alt="Hotel logo" width="100" height="100" />
       <Box component="span" mt={0.5}>
